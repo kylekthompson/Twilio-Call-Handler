b44522a68ca4009c13de436f8c6a0a390e3ed8ee
Kyle Thompson <kyle@kylekthompson.com>
2015-05-31 22:11:31 -0400

    Initial Commit

---
+++ twilio.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'twilio-ruby'
+
+get '/test' do
+  Twilio::TwiML::Response.new do |r|
+  	r.Gather :numDigits => '1', :action => '/test/handle-gather', :method => 'get' do |g|
+  		g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquires, press 2. To hear this message again, press 9.'
+  	end
+  end
+end
+
+get '/test/handle-gather' do
+	redirect '/test' unless params['Digits'] == ('1' || '2')
+	Twilio::TwiML::Response.new do |r|
+		r.Say 'Please wait while we connect you.'
+		if params['Digits'] == '1'
+			r.Dial '+13305197372', :record => 'record-from-start', :action => '/test/send-recording'
+		elsif params['Digits'] == '2'
+			r.Dial '+13302865330', :record => 'record-from-start', :action => '/test/send-recording'
+		end
+	end
+end

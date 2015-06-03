8b68b20db5c22537eb6fb33e3bdb15e54883dc24
Kyle Thompson <kyle@kylekthompson.com>
2015-06-02 22:38:10 -0400

    Woman voice

--- TwilioCallHandler.rb
+++ TwilioCallHandler.rb
@@ -4,7 +4,7 @@ require 'twilio-ruby'
 get '/' do
 	Twilio::TwiML::Response.new do |r|
   	r.Gather :numDigits => '1', :action => '/handle-gather', :method => 'get' do |g|
-  		g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquires, press 2. To hear this message again, press any other number.'
+  		g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquires, press 2. To hear this message again, press any other number.', :voice => 'woman'
   	end
   end.text
 end
@@ -12,7 +12,7 @@ end
 get '/handle-gather' do
 	redirect '/' unless params['Digits'] == ('1' || '2')
 	Twilio::TwiML::Response.new do |r|
-		r.Say 'Please wait while we connect you.'
+		r.Say 'Please wait while we connect you.', :voice => 'woman'
 		if params['Digits'] == '1'
 			r.Dial '+13305197372', :record => 'record-from-start'#, :action => '/send-recording.php'
 		elsif params['Digits'] == '2'

1ebae9bcecb90cf0c7be670245c97bf763a3c2ec
Kyle Thompson <kyle@kylekthompson.com>
2015-06-02 22:54:21 -0400

    Add stuff

--- TwilioCallHandler.rb
+++ TwilioCallHandler.rb
@@ -4,7 +4,7 @@ require 'twilio-ruby'
 get '/' do
 	Twilio::TwiML::Response.new do |r|
   	r.Gather :numDigits => '1', :action => '/handle-gather', :method => 'get' do |g|
-  		g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquires, press 2. To hear this message again, press any other number.', :voice => 'woman'
+  		g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquiries, press 2. To hear this message again, press any other number.'
   	end
   end.text
 end
@@ -12,11 +12,11 @@ end
 get '/handle-gather' do
 	redirect '/' unless params['Digits'] == ('1' || '2')
 	Twilio::TwiML::Response.new do |r|
-		r.Say 'Please wait while we connect you.', :voice => 'woman'
+		r.Say 'Please wait while we connect you.'
 		if params['Digits'] == '1'
-			r.Dial '+13305197372', :record => 'record-from-start'#, :action => '/send-recording.php'
+			r.Dial '+13305197372', :record => 'record-from-ringing', :action => '/send-recording.php'
 		elsif params['Digits'] == '2'
-			r.Dial '+13302865330', :record => 'record-from-start'#, :action => '/send-recording.php'
+			r.Dial '+13302865330', :record => 'record-from-ringing', :action => '/send-recording.php'
 		end
 	end.text
 end
---
+++ send-recording.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This section ensures that Twilio gets a response.
+ */
+header('Content-type: text/xml');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<Response></Response>'; //Place the desired response (if any) here
+
+/**
+ * This section actually sends the email.
+ */
+$to      = "christopher@onlyoptimum.com"; // Your email address
+$subject = "Recording Received! (Optimum Anesthesia Phone Service)";
+$message = "You have received a recording of a phone call through your phone service.
+Phone Call Status: {$_REQUEST['DialCallStatus']}
+Phone Call Duration: {$_REQUEST['DialCallDuration']}
+Recording URL: {$_REQUEST['RecordingUrl']}.mp3";
+$headers = "From: webmaster@onlyoptimum.com"; // Who should it come from?
+
+mail($to, $subject, $message, $headers);
+?>

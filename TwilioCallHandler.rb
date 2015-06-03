04549cd3e7c3c0655f0807382a5a140dc1882ce5
Kyle Thompson <kyle@kylekthompson.com>
2015-06-02 22:28:51 -0400

    Fix

--- TwilioCallHandler.rb
+++ TwilioCallHandler.rb
@@ -3,7 +3,7 @@ require 'twilio-ruby'
 
 get '/' do
 	Twilio::TwiML::Response.new do |r|
-  	r.Gather :numDigits => '1', :action => '/test/handle-gather', :method => 'get' do |g|
+  	r.Gather :numDigits => '1', :action => '/handle-gather', :method => 'get' do |g|
   		g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquires, press 2. To hear this message again, press 9.'
   	end
   end

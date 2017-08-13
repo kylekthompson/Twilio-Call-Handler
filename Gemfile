5a534e1bfa779b187cb078bb40328cb79063f906
Kyle Thompson <kyle@kylekthompson.com>
2017-08-12 23:14:15 -0700

    update voice and sms endpoints

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ ruby '2.4.1'
 
 source 'https://rubygems.org'
 
+gem 'sendgrid-ruby'
 gem 'sinatra'
 gem 'twilio-ruby'
--- TwilioCallHandler.rb
+++ TwilioCallHandler.rb
@@ -1,42 +1,152 @@
+require 'sendgrid-ruby'
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/optimum-voice' do
-  Twilio::TwiML::Response.new do |r|
-    r.Gather :numDigits => '1', :action => '/optimum-voice-handle-gather', :method => 'get' do |g|
-      g.Say 'You have reached Optimum Anesthesia. For requests for proposals or sales, press 1. For all other inquiries, press 2. To hear this message again, press any other number.'
-    end
-  end.text
+get '/sms/optimum' do
+  body = <<~BODY
+    You have received a message from #{params['From']}.
+    \n\n
+    Body: #{params['Body']}
+  BODY
+
+  from = SendGrid::Email.new(email: 'webmaster@onlyoptimum.com')
+  to = SendGrid::Email.new(email: 'christopher@onlyoptimum.com,kyle@kylekthompson.com')
+  subject = 'Text Received! (Optimum Anesthesia Phone Service)'
+  content = SendGrid::Content.new(type: 'text/plain', value: body)
+  mail = SendGrid::Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  sg.client.mail._('send').post(request_body: mail.to_json)
+
+  Twilio::TwiML::MessagingResponse.new do |r|
+    r.message(body: 'Thank you for contacting Optimum Anesthesia. Your message has been received and is currently being processed.')
+  end.to_s
 end
 
-get '/optimum-voice-handle-gather' do
-  redirect '/optimum-voice' unless params['Digits'] == ('1' || '2')
-  Twilio::TwiML::Response.new do |r|
-    r.Say 'Please wait while we connect you. Please note that this call will be recorded for quality assurance.'
-    if params['Digits'] == '1'
-      r.Dial '+13305388385', :record => 'record-from-ringing', :action => 'http://www.kylekthompson.com/optimumanesthesiaservices.com/optimum-voice-send-recording.php'
-    elsif params['Digits'] == '2'
-      r.Dial '+13302865330', :record => 'record-from-ringing', :action => 'https://www.kylekthompson.com/optimumanesthesiaservices.com/optimum-voice-send-recording.php'
+get '/voice/optimum' do
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.gather(numDigits: '1', action: '/voice/optimum/handle_gather', method: 'get') do |g|
+      g.say('You have reached Optimum Anesthesia.')
+      g.say('For requests for proposals or sales, press 1. For all other inquiries, press 2.')
+      g.say('To hear this message again, press any other number.')
     end
-  end.text
+  end.to_s
 end
 
-get '/mvas-voice' do
-  Twilio::TwiML::Response.new do |r|
-    r.Gather :numDigits => '1', :action => '/mvas-voice-handle-gather', :method => 'get' do |g|
-      g.Say 'You have reached Mahoning Valley Anesthesia Services. For requests for proposals or sales, press 1. For all other inquiries, press 2. To hear this message again, press any other number.'
-    end
-  end.text
+get '/voice/optimum/handle_gather' do
+  redirect '/voice/optimum' unless ['1', '2'].include?(params['Digits'])
+
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.say('Please wait while we connect you.')
+    r.say('Please note that this call will be recorded for quality assurance.')
+
+    phone_number = params['Digits'] == '1'
+      ? '+13305388385'
+      : '+13302865330'
+
+    r.dial(
+      number: phone_number,
+      record: 'record-from-ringing',
+      action: '/voice/optimum/send_voice_recording'
+    )
+  end.to_s
 end
 
-get '/mvas-voice-handle-gather' do
-  redirect '/mvas-voice' unless params['Digits'] == ('1' || '2')
-  Twilio::TwiML::Response.new do |r|
-    r.Say 'Please wait while we connect you. Please note that this call will be recorded for quality assurance.'
-    if params['Digits'] == '1'
-      r.Dial '+13305197372', :record => 'record-from-ringing', :action => 'https://www.kylekthompson.com/mvas/mvas-voice-send-recording.php'
-    elsif params['Digits'] == '2'
-      r.Dial '+13302865330', :record => 'record-from-ringing', :action => 'https://www.kylekthompson.com/mvas/mvas-voice-send-recording.php'
+post '/voice/optimum/send_voice_recording' do
+  if params['RecordingUrl']
+    body = <<~BODY
+      You have received a recording of a phone call through your phone service.
+      \n\n
+      From: #{params['From']}\n
+      Duration: #{params['DialCallDuration']} seconds\n
+      Recording URL: #{params['RecordingUrl']}.mp3\n
+    BODY
+
+    from = SendGrid::Email.new(email: 'webmaster@onlyoptimum.com')
+    to = SendGrid::Email.new(email: 'christopher@onlyoptimum.com,kyle@kylekthompson.com')
+    subject = 'Recording Received! (Optimum Anesthesia Phone Service)'
+    content = SendGrid::Content.new(type: 'text/plain', value: body)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.hangup
+  end.to_s
+end
+
+get '/sms/mvas' do
+  body = <<~BODY
+    You have received a message from #{params['From']}.
+    \n\n
+    Body: #{params['Body']}
+  BODY
+
+  from = SendGrid::Email.new(email: 'webmaster@onlyoptimum.com')
+  to = SendGrid::Email.new(email: 'christopher@onlyoptimum.com,kyle@kylekthompson.com')
+  subject = 'Text Received! (MVAS Phone Service)'
+  content = SendGrid::Content.new(type: 'text/plain', value: body)
+  mail = SendGrid::Mail.new(from, subject, to, content)
+
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  sg.client.mail._('send').post(request_body: mail.to_json)
+
+  Twilio::TwiML::MessagingResponse.new do |r|
+    r.message(body: 'Thank you for contacting Mahoning Valley Anesthesia Services. Your message has been received and is currently being processed.')
+  end.to_s
+end
+
+get '/voice/mvas' do
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.gather(numDigits: '1', action: '/voice/mvas/handle_gather', method: 'get') do |g|
+      g.say('You have reached Mahoning Valley Anesthesia Services.')
+      g.say('For requests for proposals or sales, press 1. For all other inquiries, press 2.')
+      g.say('To hear this message again, press any other number.')
     end
-  end.text
+  end.to_s
+end
+
+get '/voice/mvas/handle_gather' do
+  redirect '/voice/mvas' unless ['1', '2'].include?(params['Digits'])
+
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.say('Please wait while we connect you.')
+    r.say('Please note that this call will be recorded for quality assurance.')
+
+    phone_number = params['Digits'] == '1'
+      ? '+13305197372'
+      : '+13302865330'
+
+    r.dial(
+      number: phone_number,
+      record: 'record-from-ringing',
+      action: '/voice/mvas/send_voice_recording'
+    )
+  end.to_s
+end
+
+post '/voice/mvas/send_voice_recording' do
+    body = <<~BODY
+      You have received a recording of a phone call through your phone service.
+      \n\n
+      From: #{params['From']}\n
+      Duration: #{params['DialCallDuration']} seconds\n
+      Recording URL: #{params['RecordingUrl']}.mp3\n
+    BODY
+
+    from = SendGrid::Email.new(email: 'webmaster@mvasinc.com')
+    to = SendGrid::Email.new(email: 'cthompson@mvasinc.com,kyle@kylekthompson.com')
+    subject = 'Recording Received! (MVAS Phone Service)'
+    content = SendGrid::Content.new(type: 'text/plain', value: body)
+    mail = SendGrid::Mail.new(from, subject, to, content)
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    sg.client.mail._('send').post(request_body: mail.to_json)
+  end
+
+  Twilio::TwiML::VoiceResponse.new do |r|
+    r.hangup
+  end.to_s
 end
